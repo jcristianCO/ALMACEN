ac2b83fed12b46e7d82a50713327c541519d0128
jcristianCO <jcristiancueto@gmail.com>
2022-04-29 13:49:03 -0400

    modificar jquery

--- resources/views/entrada/index.blade.php
+++ resources/views/entrada/index.blade.php
@@ -83,7 +83,7 @@
 <script src={{  asset('js/4.4.1_bootstrap.js')}} ></script>
 {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> --}}
 
-<script src={{  asset('js/1.9.1_jquery.js')}}></script>  
+{{-- <script src={{  asset('js/1.9.1_jquery.js')}}></script>   --}}
 {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script> --}}  
 
 <script src={{ asset('js/1.19.0_jquery.validate.js') }}></script>
